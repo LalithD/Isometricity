cadadd1a6cbeb95910685245c8ed392e36db701a
TwoTau <TwoTau@users.noreply.github.com>
2015-12-20 17:09:54 -0800

    make canvas full size

--- isometry.js
+++ isometry.js
@@ -1,6 +1,30 @@
+/* jshint esnext: true */
 (function() {
     "use strict";
     window.onload = function() {
         console.log("Isometry.js loaded!");
+
+        let canvas = document.getElementById("panel");
+        let ctx = canvas.getContext("2d");
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        function makeBlock() {
+
+        }
+
+        function makeBuilding() {
+
+        }
+
+        function makeGrid() {
+            ctx.beginPath();
+            ctx.moveTo(200, 200);
+            ctx.lineTo(300, 150);
+            ctx.stroke();
+        }
+
+        makeGrid();
     };
 })();
