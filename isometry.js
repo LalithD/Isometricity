6fb1e845170df789a22e35ba5dd5a29ba19002b6
Lalith <lalith@uw.edu>
2015-12-20 17:08:13 -0800

    added random array function

--- isometry.js
+++ isometry.js
@@ -1,6 +1,17 @@
+/* jshint esnext: true */
 (function() {
     "use strict";
     window.onload = function() {
         console.log("Isometry.js loaded!");
     };
+    function makeRandArray(xSize, ySize, height) {
+        let array = [];
+        for (let i = 0; i < xSize; i++) {
+            array[i] = [];
+            for (let j = 0; j < ySize; j++) {
+                array[i][j] = Math.floor(Math.random() * (height+1));
+            }
+        }
+        return array;
+    }
 })();
